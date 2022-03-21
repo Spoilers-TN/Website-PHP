6c94a1beac15fbedbe501516cc84255ef9adf6fd
KEVIN VARGASCASTRO <101324074+KevinVargas0510@users.noreply.github.com>
2022-03-21 11:20:14 +0000

    fix

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,11 @@
 <?php
-
-  if($_SESSION["logged"]==true){
-    
-  }
-  else{
-    
-  }
-
-
+//beta-dev.spoilers.tn.it/index.php
+// Start the session
+session_start();
 ?>
+<!doctype html>
+  <html lang="it">
+    <head>
+      <title>LIBRAIMOVIC: compra-vendita di libri</title>
+    </head>
+    <body>
--- login_worker.php
+++ login_worker.php
@@ -4,7 +4,7 @@ session_start();
 
 if(isset($_SESSION["logged"])){
   echo("Welcome ". $_SESSION["nome"]);
-  echo("<p>clicca qui per tornare alla <a href='./index.php' />home</a></p>'")
+  echo("<p>clicca qui per tornare alla <a href='./index.php' />home</a></p>'");
 }else{
-  echo("dsfdsfs")
+  echo("dsfdsfs");
 }
