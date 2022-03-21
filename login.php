e334cb91c3430f59f42b8e10a156a24f3c23d13d
KEVIN VARGASCASTRO <101324074+KevinVargas0510@users.noreply.github.com>
2022-03-21 11:16:24 +0000

    test

--- login.php
+++ login.php
@@ -1,16 +1,11 @@
 <?php
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);  
-
 // Start the session
 session_start();
-
 //echo "email" .$_SESSION["email"];
-
 ?>
-
 <html>
     <head>
       <title>Login</title>
--- login_worker.php
+++ login_worker.php
@@ -6,21 +6,8 @@ if(isset($_SESSION["logged"])){
   $_SESSION["email"] = htmlentities($_POST["email"]);
   $_SESSION["password"] = htmlentities($_POST["password"]);
 
-  echo("Welcome <?php echo $_POST["email"]; ?><br>")
+  echo("Welcome ". $_SESSION["nome"]);
   echo("<p>clicca qui per tornare alla <a href='./index.php' />home</a></p>'")
 }else{
-  
+  echo "asd"
 }
-
-?>
-
-<html>
-<body>
-
-Welcome <?php echo $_POST["email"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-
-<p>clicca qui per tornare alla <a href="./index.php" />home</a></p>
-  
-</body>
-</html>
