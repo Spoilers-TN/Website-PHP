08a5aa32c4a0f7b9551631d1831c706f49e4b397
LEONARDO LOSS <101448737+leoloss@users.noreply.github.com>
2022-03-21 11:33:46 +0000

    foto

--- dashboard.php
+++ dashboard.php
@@ -13,7 +13,7 @@ session_start();
       <p>Per accedere al tuo profilo <a href="./utente.php" />clicca qui.</a></p>
       <h3>Qui puoi cercare i tuoi libri!</h3>
       <input type="text" id="libro" placeholder="" name="libro">
-      <img src= ""
+      <img src= "https://www.google.com/url?sa=i">
       <button type="submit" name="libro">Cerca</button>
       <p>clicca qui per tornare alla <a href="./index.php" />home</a></p>
     </body>
