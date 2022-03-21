c0b549ff6f302221e06ccdec093dc761abb92c0c
LEONARDO LOSS <>
2022-03-21 09:54:05 +0000

    prova

--- utente.php
+++ utente.php
@@ -1,6 +1,6 @@
 <?php
 //session_start();
-    echo "Welcome:" .$_SESSION["email"]."<br/>"
+    echo "Welcome:" "".$_SESSION["email"]."<br/>"
     echo "Questi sono i tuoi dati:"."<br/>";
     echo "Nome:" .$_SESSION["nome"]."<br/>";
     echo "Cognome:".$_SESSION["cognome"]."<br/>";
