843dba7ba2e0f4e006c9ab6daeef79211de7c472
LEONARDO LOSS <101448737+leoloss@users.noreply.github.com>
2022-03-21 11:36:23 +0000

    foto

--- dashboard.php
+++ dashboard.php
@@ -13,8 +13,8 @@ session_start();
       <p>Per accedere al tuo profilo <a href="./utente.php" />clicca qui.</a></p>
       <h3>Qui puoi cercare i tuoi libri!</h3>
       <input type="text" id="libro" placeholder="" name="libro">
-      <img src= "https://www.google.com/url?sa=i">
       <button type="submit" name="libro">Cerca</button>
       <p>clicca qui per tornare alla <a href="./index.php" />home</a></p>
+      <img src="https%3A%2F%2Fgithub.com%2Fadamhub%2Fnebri">
     </body>
   </html>
