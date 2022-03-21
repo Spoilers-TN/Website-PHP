d1c5b7c52371a98de150ebb41b09d56ee29325df
Roberto Pozzi <87300995+SDENTAT0@users.noreply.github.com>
2022-03-21 10:31:23 +0000

    alaska

--- index.php
+++ index.php
@@ -16,8 +16,10 @@ session_start();
         <p>clicca qui per la <a href="./register.php" />registrazione</a></p>
       <p>Per accedere al nostro sito Web <a href="https://www.spoilers.tn.it" target="_blank" />clicca qui.        </a></p>
       <p>clicca qui per chiudere la <a href="./close_session.php" />sessione</a></p>
-      <?php if(isset($_SESSION["name"])) : ?>
-      <p>Sei in una sessione</p>
-      <? endif ?>
+      <?php if(isset($_SESSION["name"])){
+        echo("<p>Sei in una sessione</p>");
+      }
+       
+      ?>
     </body>
   </html>
