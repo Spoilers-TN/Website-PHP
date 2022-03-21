9398704807d8dc294a0d22acd3976324f8d170f0
KEVIN VARGASCASTRO <101324074+KevinVargas0510@users.noreply.github.com>
2022-03-21 11:22:17 +0000

    aggiunto link

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,3 @@
-<?php
 //beta-dev.spoilers.tn.it/index.php
 // Start the session
 session_start();
@@ -9,3 +8,11 @@ session_start();
       <title>LIBRAIMOVIC: compra-vendita di libri</title>
     </head>
     <body>
+      <h1>LIBRAIMOVIC</h1>
+      <h3>Qui puoi cercare i tuoi libri!</h3>
+      <p>Per accedere al tuo profilo <a href="./utente.php" />clicca qui.</a></p>
+      <input type="i" id="password" placeholder="password" name="password">
+      <button type="submit" name="login">Accedi</button>
+
+    </body>
+  </html>
--- login_worker.php
+++ login_worker.php
@@ -6,5 +6,5 @@ if(isset($_SESSION["logged"])){
   echo("Welcome ". $_SESSION["nome"]);
   echo("<p>clicca qui per tornare alla <a href='./index.php' />home</a></p>'");
 }else{
-  echo("dsfdsfs");
+  echo("Non sei registrato clicca <a href='./register.php' />qui</a> per registrarti");
 }
