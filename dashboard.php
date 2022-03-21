b3c23ef391c2b2465741720d4d06f377d78fc1d1
LEONARDO LOSS <101448737+leoloss@users.noreply.github.com>
2022-03-21 11:36:56 +0000

    fix

--- dashboard.php
+++ dashboard.php
@@ -15,6 +15,5 @@ session_start();
       <input type="text" id="libro" placeholder="" name="libro">
       <button type="submit" name="libro">Cerca</button>
       <p>clicca qui per tornare alla <a href="./index.php" />home</a></p>
-      <img src="https%3A%2F%2Fgithub.com%2Fadamhub%2Fnebri">
     </body>
   </html>
