366b15ba9a297ac0501c3a28de19f8d7f460ad6e
Francesco Masala <25754886+francescomasala@users.noreply.github.com>
2022-03-21 09:40:10 +0000

    Updated

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);  
 
 // Start the session
-//session_start();
+session_start();
 
 //echo "email" .$_SESSION["email"];
 
@@ -18,7 +18,7 @@ error_reporting(E_ALL);
       <title>Login</title>
     </head>
     <body>
-      <?php if(isset($_SESSION["email"])): ?>
+      <?php if(!isset($_SESSION["email"])): ?>
         
         <form method="post" action="./login_worker.php">
             <h1>Login</h1>
