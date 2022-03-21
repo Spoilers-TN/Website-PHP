68e6c106a2d94fe332b11b68baeb61f2a695fe0d
MIRCO SOTTOVIA <101448666+mirksottovia@users.noreply.github.com>
2022-03-21 10:16:41 +0000

    gdf

--- close_session.php
+++ close_session.php
@@ -1,6 +1,6 @@
 <?php
 
-session_close();
+session_write_close()
 
 header("location: /index.php");
 
--- utente.php
+++ utente.php
@@ -1,11 +1,11 @@
 <?php
 //inizio sessione
 session_start();
-    echo "Welcome: " .$_SESSION["email"]."</br>";
+    echo "Welcome: " .$_SESSION["nome"].$_SESSION["cognome"]"</br>";
     echo "Questi sono i tuoi dati:"."</br>";
     echo "Nome: " .$_SESSION["nome"]."</br>";
     echo "Cognome: ".$_SESSION["cognome"]."</br>";
     echo "Classe: ".$_SESSION["classe"]."</br>";
+    echo "Email: ".$_SESSION["email"]."</br>";
     echo "Data di nascita: ".$_SESSION["data"]."</br>";
 ?>
-
