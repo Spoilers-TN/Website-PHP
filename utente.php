e89f43a151ad02f62d20d0ff4c20e672d7ec1782
Francesco Masala <25754886+francescomasala@users.noreply.github.com>
2022-03-21 09:57:50 +0000

    Updated some files

--- utente.php
+++ utente.php
@@ -2,8 +2,8 @@
 session_start();
     echo "Welcome: " .$_SESSION["email"]."<br/>";
     echo "Questi sono i tuoi dati:"."<br/>";
-    echo "Nome:" .$_SESSION["nome"]."<br/>";
-    echo "Cognome:".$_SESSION["cognome"]."<br/>";
-    echo "Classe:".$_SESSION["classe"]."<br/>";
+    echo "Nome: " .$_SESSION["nome"]."<br/>";
+    echo "Cognome: ".$_SESSION["cognome"]."<br/>";
+    echo "Classe: ".$_SESSION["classe"]."<br/>";
     echo "Data di nascita: ".$_SESSION["data"]."<br/>";
 ?>
