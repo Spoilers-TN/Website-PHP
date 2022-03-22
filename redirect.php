bdf20019d470ff61af1d0822dab8396747dc596d
Alessio Cristoforetti <101449185+AleCristo@users.noreply.github.com>
2022-03-22 11:36:34 +0000

    tet

--- redirect.php
+++ redirect.php
@@ -1,11 +1,15 @@
 <?php
 require_once 'vendor/autoload.php';
-  
+
+  echo "1";
+
 // init configuration
 $clientID = '750408780393-lsn833luim1lqd11dtpnuscil3ast00e.apps.googleusercontent.com';
 $clientSecret = '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';
 $redirectUri = 'https://beta-dev.spoilers.tn.it/redirect.php';
-   
+
+  echo "2";
+
 // create Client Request to access Google API
 $client = new Google_Client();
 $client->setClientId($clientID);
@@ -13,21 +17,27 @@ $client->setClientSecret($clientSecret);
 $client->setRedirectUri($redirectUri);
 $client->addScope("email");
 $client->addScope("profile");
-  
+
+  echo "3";
+
 // authenticate code from Google OAuth Flow
 if (isset($_GET['code'])) {
  $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
  $client->setAccessToken($token['access_token']);
-   
+
+  echo "4";
+  
  // get profile info
  $google_oauth = new Google_Service_Oauth2($client);
  $google_account_info = $google_oauth->userinfo->get();
  $email =  $google_account_info->email;
  $name =  $google_account_info->name;
 
-  header("locatrion: ./login_worker.php");
+  echo "5";
  // now you can use this profile info to create account in your website and make user logged in.
 } else {
+  echo "6";
   header("location: ".$client->createAuthUrl());
+  echo "7";
 }
 ?>
