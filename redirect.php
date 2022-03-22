feee4f7992533123bed10e5ea6846b80a2a71aca
Roberto Pozzi <87300995+SDENTAT0@users.noreply.github.com>
2022-03-22 10:46:19 +0000

    Updated

--- redirect.php
+++ redirect.php
@@ -27,6 +27,6 @@ if (isset($_GET['code'])) {
   
  // now you can use this profile info to create account in your website and make user logged in.
 } else {
- echo "<a href='".$client->createAuthUrl()."'>Google Login</a>";
+  window.location.href = '".$client->createAuthUrl()."';
 }
 ?>
