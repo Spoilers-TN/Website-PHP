d0b761e54ae8fc62e99edee4f8991b389719bdd0
Roberto Pozzi <87300995+SDENTAT0@users.noreply.github.com>
2022-03-23 08:22:45 +0000

    ripristino modifiche per prova login

--- index.php
+++ index.php
@@ -18,14 +18,12 @@
       <p>Ciao! Siamo la 4ina e abbiamo redatto questo sito per la <strong>compra-vendita</strong> dei libri scolastici.</p>
       <p>Clicca qui per il <a href="./login.php" />login</a></p>
       <p>Per accedere al nostro sito Web <a href="https://www.spoilers.tn.it" target="_blank" />clicca qui.</a></p>
-      <p>Per accedere al tuo profilo <a href="./utente.php" />clicca qui.</a></p>
-      <p>Clicca qui per chiudere la <a href="./close_session.php" />sessione</a></p>
       
       <?php
         if(isset($_SESSION["logged"])){
-          //echo "<p>Per accedere al tuo profilo <a href=\"./utente.php\" />clicca qui.</a></p>";
+          echo "<p>Per accedere al tuo profilo <a href=\"./utente.php\" />clicca qui.</a></p>";
           echo("<p>Sei in una sessione!</p>");
-          //echo "<p>Clicca qui per chiudere la <a href=\"./close_session.php\" />sessione</a></p>";
+          echo "<p>Clicca qui per chiudere la <a href=\"./close_session.php\" />sessione</a></p>";
         }else{
           echo("<p>non sei in una sessione!</p>");
         }
