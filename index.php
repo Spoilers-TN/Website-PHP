8c8a8bd5b5bfa4bc571a3bd87a8ea72a17f15aac
Alessio Cristoforetti <101449185+AleCristo@users.noreply.github.com>
2022-03-23 07:35:23 +0000

    test login 2..0

--- index.php
+++ index.php
@@ -1,27 +1,33 @@
 <?php
-//beta-dev.spoilers.tn.it/index.php
-// Start the session
-session_start();
+  //beta-dev.spoilers.tn.it/index.php
+  // Start the session
+  session_start();
 ?>
+
 <!doctype html>
   <html lang="it">
     <head>
       <title>LIBRAIMOVIC: compra-vendita di libri</title>
     </head>
     <body>
+      
       <h1>LIBRAIMOVIC</h1>
+      
       <h3>Created by 4INA!</h3>
+      
       <p>Ciao! Siamo la 4ina e abbiamo redatto questo sito per la <strong>compra-vendita</strong> dei libri scolastici.</p>
       <p>Clicca qui per il <a href="./login.php" />login</a></p>
-       <p>Per accedere al tuo profilo <a href="./utente.php" />clicca qui.</a></p>
+      <p>Per accedere al tuo profilo <a href="./utente.php" />clicca qui.</a></p>
       <p>Per accedere al nostro sito Web <a href="https://www.spoilers.tn.it" target="_blank" />clicca qui.        </a></p>
       <p>Clicca qui per chiudere la <a href="./close_session.php" />sessione</a></p>
-      <?php if(isset($_SESSION["logged"])){
-        echo("<p>Sei in una sessione!</p>");
-      }else{
-        echo("<p>non sei in una sessione!</p>");
-      }
-       
+      
+      <?php 
+        if(isset($_SESSION["logged"])){
+          echo("<p>Sei in una sessione!</p>");
+        }else{
+          echo("<p>non sei in una sessione!</p>");
+        }
       ?>
+
     </body>
   </html>
--- register_worker.php
+++ register_worker.php
@@ -1,23 +1,12 @@
 <?php
 session_start();
-/*
+
 $_SESSION["nome"] = $_POST["nome"];
 $_SESSION["cognome"] = $_POST["cognome"];
 $_SESSION["classe"] = $_POST["classe"];
 $_SESSION["email"] = $_POST["email"];
-$_SESSION["password"] = $_POST["password"];
-$_SESSION["data"] = $_POST["data"];
 $_SESSION["logged"] = true;
-$_SESSION["registed"] = true;
-*/
 
-$_SESSION["nome"] = "mario";
-$_SESSION["cognome"] = "rossi";
-$_SESSION["classe"] = "4ina";
-$_SESSION["email"] = "mario.rossi@mail.com";
-$_SESSION["password"] = "sium";
-$_SESSION["data"] = "21/03/2022";
-$_SESSION["logged"] = true;
 
 header("location: /dashboard.php");
 ?>
