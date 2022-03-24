f45800056cd23eccaaf63f13e7c7bc8b4c52661c
Alessio Cristoforetti <101449185+AleCristo@users.noreply.github.com>
2022-03-24 07:23:26 +0000

    undo

--- close_session.php
+++ close_session.php
@@ -1,16 +1,8 @@
 <?php
-
-session_start();
-
-// Remove token and user data from the session
-unset($_SESSION['access_token']);
-
-// Reset OAuth access token
-$gClient->revokeToken();
-
-// Destroy entire session data
-session_destroy();
   
-header("location: /index.php?logout=ok");
+  session_start();
+  session_destroy();
+  
+  header("location: /index.php?logout=ok");
 
 ?>
