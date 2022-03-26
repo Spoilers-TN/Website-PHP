17e0c1a6d15face09591cc09383e31cc93f3e1e0
KEVIN VARGASCASTRO <101324074+KevinVargas0510@users.noreply.github.com>
2022-03-26 10:15:14 +0000

    test

--- Public/settings.php
+++ Public/settings.php
@@ -40,32 +40,22 @@
           if(isset($_POST["save_bio"])){
             $_SESSION["biografia"] = $_POST["biografia"];
             $query = "UPDATE INTO Users (User_Biog) WHERE User_ID='".$_SESSION["email"]."' SET User_Biog='".$_SESSION["biografia"]."'";
-            $insert = mysqli_query($db_conn, $query);
+            //$insert = mysqli_query($db_conn, $query);
             if ($db_conn->query($query) === TRUE) {
               echo "Record updated successfully";
             } else {
               echo "Error updating record: " . $db_conn->error;
-          }
+            }
           }
           //mysqli_query($db_conn, "SELECT User_Biog FROM Users WHERE User_ID='".$_SESSION["email"]."'");
         ?>
 
       </div>
     </form>
-    <br>
-  
-    <!--bottone salva-->
-    <!--<div class="container">
-    <div class="row">
-                          <div class="col">
-                            <div class="float-right">
-                              
-                              
-                              <button type="submit" class="btn btn-warning" data-toggle="modal" data-target="#saveButton" name="salva">
-                                Salva modifiche
-                              </button>-->
-                            
   </div>
+  
+  <br>
+  
   <div>
     <!-- Modal -->
     <div class="modal fade" id="saveButton" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="saveChanges" aria-hidden="true">
