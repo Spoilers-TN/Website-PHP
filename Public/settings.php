762199c492bb97acf646cffb24db9a36d2bdd3a8
Roberto Pozzi <87300995+SDENTAT0@users.noreply.github.com>
2022-03-26 09:49:02 +0000

    fix

--- Public/settings.php
+++ Public/settings.php
@@ -39,9 +39,8 @@
         <?php
           $_SESSION["biografia"] = $_POST["biografia"];
           if(isset($_POST["save_bio"])){
-            $query = "UPDATE INTO Users (User_Biog) WHERE User_ID='".$_SESSION["email"]."' SET User_Biog='".$_SESSION["biografia"]."'";
+            $query = "UPDATE INTO Users (User_Biog) WHERE User_ID='".$_SESSION["email"]."' SET User_Biog='".$_SESSION["save_bio"]."'";
             $insert = mysqli_query($db_conn, $query);
-            echo $_SESSION["biografia"];
           }
           else{
             echo "Non va :)";
