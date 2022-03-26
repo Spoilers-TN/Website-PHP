af5fcfa5bae6f2cc10867bec8be14e3a3dbf420a
Roberto Pozzi <87300995+SDENTAT0@users.noreply.github.com>
2022-03-26 09:40:41 +0000

    fix

--- Public/settings.php
+++ Public/settings.php
@@ -6,47 +6,60 @@
 
 <!doctype html>
 <html>
-  <form>
-
-<div class="card">
-  <div class="card-body">
-    <h4 class="card-title">Modifica profilo</h4>
-  </div>
-  <div class="form-group">
-    <label>Dati anagrafici</label>
-    <input type="email" class="form-control" readonly placeholder=<?php echo $_SESSION["email"]; ?>>
-  </div>
-  <div class="row">
-    <div class="col">
-      <input type="text" class="form-control" readonly placeholder=<?php echo $_SESSION["nome"] ?>>
+  <div class="card">
+    <div class="card-body">
+      <h4 class="card-title">Modifica profilo</h4>
     </div>
-    <div class="col">
-      <input type="text" class="form-control" readonly placeholder=<?php echo $_SESSION["cognome"] ?>>
-    </div>
-  </div>
-
-  <br>
-  <form method="POST" action="https://beta-dev.spoilers.tn.it/bio_worker.php">
-    
-    <label>Imposta la tua biografia.</label>
-    
     <div class="form-group">
-      
-      <input type="text" class="form-control" rows="3" id="biografia" name="biografia" placeholder="Biografia" required>
-      
-      
-      <button type="submit" name="save_bio" id="save_bio">
-        Salva
-      </button>
-
-      <?php echo "<br>La tua biografia:<br>".$_SESSION["biografia"]; ?>
+      <label>Dati anagrafici</label>
+      <input type="email" class="form-control" readonly placeholder=<?php echo $_SESSION["email"]; ?>>
     </div>
-  </form>
-  <br>
+    <div class="row">
+      <div class="col">
+        <input type="text" class="form-control" readonly placeholder=<?php echo $_SESSION["nome"] ?>>
+      </div>
+      <div class="col">
+        <input type="text" class="form-control" readonly placeholder=<?php echo $_SESSION["cognome"] ?>>
+      </div>
+    </div>
+  
+    <br>
+    <form method="POST">
+      
+      <label>Imposta la tua biografia.</label>
+      
+      <div class="form-group">
+        
+        <input type="text" class="form-control" rows="3" id="biografia" name="biografia" placeholder="Biografia" required>
+        
+        
+        <button type="submit" name="save_bio" id="save_bio">
+          Salva
+        </button>
+  
+        <?php
 
-  <!--bottone salva-->
-  <!--<div class="container">
-<div class="row">
+          $_SESSION["biografia"] = $_POST["biografia"];
+
+          if(isset($_SESSION["biografia"])){
+            $query = "UPDATE INTO Users (User_Biog) WHERE User_ID='".$_SESSION["email"]."' SET User_Biog='".$_SESSION["biografia"]."'";
+            $insert = mysqli_query($db_conn, $query);
+            echo $_SESSION["biografia"];
+          }
+          else{
+            echo "Non va :)";
+          }
+
+          echo "<br>La tua biografia:<br>".$_SESSION["biografia"];
+        ?>
+
+      </div>
+    </form>
+    <br>
+  
+    <!--bottone salva-->
+    <!--<div class="container">
+    <div class="row">
                           <div class="col">
                             <div class="float-right">
                               
@@ -55,33 +68,28 @@
                                 Salva modifiche
                               </button>-->
                             
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <!-- Modal -->
-                      <div class="modal fade" id="saveButton" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="saveChanges" aria-hidden="true">
-                        <div class="modal-dialog">
-                          <div class="modal-content">
-                            <div class="modal-header">  
-                              <h5 class="modal-title" id="saveChanges">Salvare modifiche</h5>
-                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                              </button>
-                            </div>
-                            <div class="modal-body">
-                              Sei sicuro di voler salvare le modifiche?
-                            </div>
-                            <div class="modal-footer">
-                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Annulla</button>
-                              <a class="btn btn-warning" href="./dashboard.html">Sì</a>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                  </div>
-            </div>
+  </div>
+  <div>
+    <!-- Modal -->
+    <div class="modal fade" id="saveButton" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="saveChanges" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">  
+            <h5 class="modal-title" id="saveChanges">Salvare modifiche</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
           </div>
+          <div class="modal-body">
+            Sei sicuro di voler salvare le modifiche?
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Annulla</button>
+            <a class="btn btn-warning" href="./dashboard.html">Sì</a>
+          </div>
+        </div>
       </div>
-
+    </div>
+  </div>
+  
 </html>
