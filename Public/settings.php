bfbde7cb2b5c5b0b3c27f2bc84f8eabdeddf670c
KEVIN VARGASCASTRO <101324074+KevinVargas0510@users.noreply.github.com>
2022-03-26 08:42:51 +0000

    test

--- Public/settings.php
+++ Public/settings.php
@@ -34,9 +34,11 @@
                           <button type="submit" name="save_bio" id="save_bio">
                             Salva
                           </button>
-<?php $query = "INSERT INTO users (User_Biog)"."VALUES('".$_POST["save_bio"]."')";
-
-                    $insert = mysqli_query($db_conn, $query);?></br>
+                <?php 
+                    if(isset($_POST["biografia"])){
+                      $query = "INSERT INTO users (User_Biog)"."VALUES('".$_POST["biografia"]."')";
+  
+                      $insert = mysqli_query($db_conn, $query);?></br>
                         </div>
 
                       <!--bottone salva-->
